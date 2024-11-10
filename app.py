cfa294add9d2b6fdf78bb61f201b5cd7993bb49d
mauyaa <mauyabevan@gmail.com>
2024-11-10 09:10:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+
 
 # Page configuration
 st.set_page_config(page_title="Home Energy Management System", layout="wide")
