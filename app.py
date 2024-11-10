b8681df027b6afd0212ad3ade0a0f594723d1a67
mauyaa <mauyabevan@gmail.com>
2024-11-10 09:11:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # Page configuration
 st.set_page_config(page_title="Home Energy Management System", layout="wide")
